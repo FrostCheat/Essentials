20f7811bd12e4e5d3e5328ea92886389903b7cbe
FrostCheat <123274033+FrostCheat@users.noreply.github.com>
2025-06-22 17:33:34 -0500

    Update EventListener.php

--- src/frostcheat/essentials/EventListener.php
+++ src/frostcheat/essentials/EventListener.php
@@ -5,12 +5,14 @@ namespace frostcheat\essentials;
 use frostcheat\essentials\sessions\Session;
 use frostcheat\essentials\sessions\SessionManager;
 use frostcheat\essentials\utils\ReflectionUtils;
+use pocketmine\event\entity\EntityTeleportEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerLoginEvent;
 use pocketmine\event\player\PlayerPreLoginEvent;
 use pocketmine\network\mcpe\protocol\GameRulesChangedPacket;
 use pocketmine\network\mcpe\protocol\types\BoolGameRule;
+use pocketmine\player\Player;
 use pocketmine\player\PlayerInfo;
 
 class EventListener implements Listener {
@@ -43,6 +45,16 @@ class EventListener implements Listener {
         if ((bool) Loader::getInstance()->getConfig()->get("showCoordinates", true)) {
             $pk = GameRulesChangedPacket::create(['showCoordinates' => new BoolGameRule(true, false)]);
             $player->getNetworkSession()->sendDataPacket($pk);
+        }
+    }
+
+    public function onTeleport(EntityTeleportEvent $event): void {
+        $entity = $event->getEntity();
+        if ($entity instanceof Player) {
+            $session = SessionManager::getInstance()->getSession($entity->getName());
+            if ($session !== null) {
+                $session->setLastPosition($event->getFrom());
+            }
         }
     }
 }
